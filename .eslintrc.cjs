a1aa566c3d8e5877eec204bc894e49026e256a9a
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-03-08 19:15:02 +0100

    fix: RSS-PZ-00 Fix config problems

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    'prettier/prettier': ['error'],
+    'prettier/prettier': ['error', {"endOfLine": "auto"}],
   },
   overrides: [
     {
