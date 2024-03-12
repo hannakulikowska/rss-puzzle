2b3ea64a8981def537fa62474c3825d7577fb9ac
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-03-12 10:31:42 +0100

    feat: RSS-PZ-03 Save user entry data to Local Storage (#6)

---
+++ src/utils/FormSubmitHandler.ts
@@ -0,0 +1,20 @@
+export default class FormSubmitHandler {
+  constructor(
+    private formElement: HTMLElement,
+    private firstNameField: HTMLInputElement,
+    private surnameField: HTMLInputElement
+  ) {
+    this.setupSubmitListener();
+  }
+
+  private setupSubmitListener(): void {
+    this.formElement.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const userData = {
+        firstName: this.firstNameField.value,
+        surname: this.surnameField.value,
+      };
+      localStorage.setItem('userEntry', JSON.stringify(userData));
+    });
+  }
+}
